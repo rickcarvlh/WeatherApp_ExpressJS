cfc8f9bcc68b52f2346c33f76fdcca576b123d3a
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-08-02 15:47:53 +0100

    search form /  xhr requests

--- public/js/app.js
+++ public/js/app.js
@@ -6,13 +6,34 @@ console.log('Client side js is loaded');
 //   });
 // });
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
+// fetch('http://localhost:3000/weather?address=boston').then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log(data.error);
+//     } else {
+//       console.log(data.location);
+//       console.log(data.forecast);
+//     }
+//   });
+// });
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const location = search.value;
+  fetch('http://localhost:3000/weather?address=' + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          console.log(data);
+          console.log(data.location);
+          console.log(data.forecast);
+        }
+      });
     }
-  });
+  );
 });
